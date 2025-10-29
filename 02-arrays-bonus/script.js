2d5c77f68e3a0bfad9f01fc998ac8d4f82779ebf
DavidZarfati <zarda2001@gmail.com>
2025-10-29 16:46:35 +0100

    feat:fine bonus con i cicli

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,23 +10,59 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = teachers.reverse();
+// const reversedTeachers = teachers.reverse();
+// console.log(reversedTeachers)
+let reversedTeachers = []
+for (let i = teachers.length - 1; i >= 0; i--) {
+  reversedTeachers.push(teachers[i]);
+}
 console.log(reversedTeachers)
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = teachers.filter((teacher) => teacher.length >= 5)
+// const longNames = teachers.filter((teacher) => teacher.length >= 5)
+// console.log(longNames)
+let longNames = []
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i].length >= 5) {
+    longNames.push(teachers[i])
+  }
+}
 console.log(longNames)
 
+
 // 3. Rimuovi 'Ed' dall'array teachers
-teachers.splice(5, 1)
+// teachers.splice(5, 1)
+// console.log(teachers)
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i] === "Ed") {
+    teachers.splice(i, 1)
+  }
+}
 console.log(teachers)
+
+
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = teachers.includes('Fabio');
-console.log(isFabioPresent);
+// const isFabioPresent = teachers.includes('Fabio');
+// console.log(isFabioPresent);
+let isFabioPresent = false
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i] === "Fabio") {
+    isFabioPresent = true
+  }
+}
+console.log(isFabioPresent)
+
+
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = teachers.join(',');
-console.log(teachersString)
+// const teachersString = teachers.join(',');
+// console.log(teachersString)
+let messaggio = ""
+for (let i = 0; i < teachers.length - 1; i++) {
+  messaggio = `${messaggio}${teachers[i]},`
+}
+messaggio = `${messaggio}${teachers[teachers.length - 1]}`
+console.log(messaggio)
